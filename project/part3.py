3abf8b72d31ef0dbcb2122ccb5700cd14f3fa4e7
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-12-13 00:11:53 +0800

    implement part3 testing and generate outputs

--- project/part3.py
+++ project/part3.py
@@ -5,78 +5,15 @@
 import utilities
 import math
 import sys
-
-## This is code to obtain the transmission parameters
-
-# Outputs a dictionary {'label1': {'label1': count of label1->label1, 'label2': count of label1->label2...}...}
-def count_transmissions(data):
-    transmissions = {'START':{}, 'O':{},'B-positive':{},'B-neutral':{},'B-negative':{},'I-positive':{},'I-neutral':{},'I-negative':{}}
-    for label in transmissions.keys():
-        transmissions[label] = {'O':0,'B-positive':0,'B-neutral':0,'B-negative':0,'I-positive':0,'I-neutral':0,'I-negative':0, 'STOP':0}
-    last_position = 'START'
-    for line in data:
-        if line =="\n": #if line is a slash n - need to concern, rewrite processing script to include \n
-            transmissions[last_position]["STOP"] += 1 #add transmission from last read label to STOP state
-            last_position = 'START' #reset state back to start
-        else:
-            transmissions[last_position][line[1]] += 1 #for transmission from the Label/State at LAST POSITION to the state read from the line
-            last_position = line[1] #move/transit to the next state read from the line
-    #transmissions[last_position]['STOP'] += 1 #add +1 for each transmission from label/state read to STOP
-    return transmissions
-
-# Outputs a dictionary {'label1': {'label1': probability of label1->label1, 'label2': probability of label1->label2...}...}
-def estimate_transmission_parameters(transmission_count, tags_count):
-    transmission_prob = {'START':{}, 'O':{},'B-positive':{},'B-neutral':{},'B-negative':{},'I-positive':{},'I-neutral':{},'I-negative':{},'STOP':{}}
-    for label in transmission_prob.keys():
-        transmission_prob[label] = {'START':0,'O':0,'B-positive':0,'B-neutral':0,'B-negative':0,'I-positive':0,'I-neutral':0,'I-negative':0, 'STOP':0}
-    for label_in, t_counts in transmission_count.items():
-        for label_out, count in t_counts.items():
-            transmission_prob[label_in][label_out] = count/tags_count[label_in]
-    """
-    #Special Cases
-    for first_label, start_count in transmission_count['START'].items():
-        if start_count > 0:
-            transmission_prob['START'][first_label] = 1
-    """
-    return transmission_prob
-
-def estimate_emission_parameters_with_unk(count_tags, count_tag_words,k=1):
-  all_estimations = {} #dictionary
-  for unique_tag_tuple in count_tag_words.items():
-    single_tag_estimation = {} #dictionary
-    for word_count in unique_tag_tuple[1].items():
-      estimated_value = word_count[1]/(count_tags[unique_tag_tuple[0]] + k) #this is the label y count
-      single_tag_estimation[word_count[0]] = estimated_value
-    single_tag_estimation['#UNK#'] = k/(count_tags[unique_tag_tuple[0]] + k)
-    all_estimations[unique_tag_tuple[0]] = single_tag_estimation
-  return all_estimations
-
-# Input: list of lists containing word, label, with "\n" delimiting new lines. Output of read_data_transmission.
-# Output: list of documents (which are lists containing word, label). 3-layer listing.
-def separate_documents(data):
-    out = [[]]
-    for i in data:
-        if i == '\n':
-            out.append([])
-        else:
-            out[-1].append(i)
-
-    # To get rid of the last empty array
-    return out[:-1]
-
-def get_training_set_words(data):
-    words = set()
-    for i in data:
-        if len(data) > 1:
-            words.add(i[0])
-    return words
+import part1
+import part2
 
 # Viterbi algorithm to predict output labels on each document.
 # Note: should be called on EACH DOCUMENT of the VALIDATION/TEST set (data is a list of list containing strings)
 # a(u,v) is t_params
 # b(u,o) is e_params
 
-def viterbi(data, t_params, e_params, word_set):
+def viterbi_5th(data, t_params, e_params, word_set):
 
     # print("Beginning viterbi for", data)
     n = len(data)
@@ -225,45 +162,56 @@ def viterbi(data, t_params, e_params, word_set):
             all_viterbi_list = all_viterbi_list[:5]
         # print(all_viterbi_list)
         
-    output = all_viterbi_list[4][1]
+    output = all_viterbi_list[4][1] #5TH BEST OUTPUT SEQUENCE
     if len(output) < n+2:
         output = output + ['O']*(n+1 - len(output)) + ["STOP"]
     
     return output[1:-1] #access the 5TH BEST sequence
 
-def viterbi_loop(separated, t_params, e_params, word_set):
+def viterbi_loop_5th(separated, t_params, e_params, word_set):
     final = []
     for doc in separated:
-        final.append(viterbi(doc, t_params, e_params, word_set))
+        final.append(viterbi_5th(doc, t_params, e_params, word_set))
     return final
 
-# Input: dev_set (list of lists of strings)
-def output_prediction(prediction, data, path):
-    assert(len(prediction) == len(data))
-    file = open(path, "w", encoding="utf-8")
-    n = len(data)
-    print("Writing", n, "lines")
-    for i in range(n):
-        assert(len(data[i]) == len(prediction[i]))
-        m = len(data[i])
-        for j in range(m):
-            file.write(data[i][j] + " " + prediction[i][j] + "\n")
-        file.write("\n")
-    print("Wrote predictions to", path)
+def run_viterbi_5th(training_path, test_path, output_path):
+    train = utilities.read_data_transmission(training_path)
+    train_words = utilities.get_training_set_words(train)
+    test = utilities.read_dev(test_path)
+    tags = utilities.count_tags_transmission(train)
+    tag_words = utilities.count_tag_words(train)
+    transmission_counts = part2.count_transmissions(train)
+    t_params =  part2.estimate_transmission_parameters(transmission_counts, tags)
+    e_params = part1.estimate_emission_parameters_with_unk(tags, tag_words)
+    prediction = viterbi_loop_5th(test, t_params, e_params, train_words)
+    utilities.output_prediction(prediction, test, output_path)
 
+
+## Actual viterbi calls
+if __name__ == '__main__':
+    n = len(sys.argv)
+
+    if n == 1:
+        run_viterbi_5th(r"ES/train", r"ES/dev.in", r"ES/dev.p3.out")
+        run_viterbi_5th(r"RU/train", r"RU/dev.in", r"RU/dev.p3.out")
+    else:
+        if n == 4:
+            run_viterbi_5th(sys.argv[1], sys.argv[2], sys.argv[3])
+        else:
+            print("usage: python part3.py [train_path] [test_path] [output_path]")
 
 ## Actual running code
 #Outputs list (size n) of list (size 2) in this form: ['word', 'label']
-es_train = utilities.read_data_transmission(r"ES\train")
-train_words = get_training_set_words(es_train)
-es_dev = utilities.read_dev(r"ES\dev.in")
-# print(es_dev)
-# separated = separate_documents(es_train)
-tags = utilities.count_tags_transmission(es_train)
-tag_words = utilities.count_tag_words(es_train)
-transmission_counts = count_transmissions(es_train)
-t_params = estimate_transmission_parameters(transmission_counts, tags)
-e_params = estimate_emission_parameters_with_unk(tags, tag_words)
+# es_train = utilities.read_data_transmission(r"ES\train")
+# train_words = get_training_set_words(es_train)
+# es_dev = utilities.read_dev(r"ES\dev.in")
+# # print(es_dev)
+# # separated = separate_documents(es_train)
+# tags = utilities.count_tags_transmission(es_train)
+# tag_words = utilities.count_tag_words(es_train)
+# transmission_counts = count_transmissions(es_train)
+# t_params = estimate_transmission_parameters(transmission_counts, tags)
+# e_params = estimate_emission_parameters_with_unk(tags, tag_words)
 
 # file = open("e_params", "w", encoding="utf-8")
 # file.write(str(e_params))
@@ -271,13 +219,13 @@ e_params = estimate_emission_parameters_with_unk(tags, tag_words)
 # ru_train = utilities.read_data_transmission(r"RU\train")
 
 # # Testing viterbi
-test = ['Con', 'lo', 'cual', 'en', 'el', 'comedor', 'tienes', 'que', 'levantar', 'mas', 'la', 'voz', 
-'para', 'oirte', 'y', 'se', 'forma', 'un', 'ambiente', 'que', 'no', 'lo', 'que', 'se', 'espera', 'de', 'una', 'estrella', 'michelin', '.']
-output_sequence = viterbi(test, t_params, e_params, train_words)
-print('\n')
-print(output_sequence)
-print(len(test))
-print(len(output_sequence))
+# test = ['Con', 'lo', 'cual', 'en', 'el', 'comedor', 'tienes', 'que', 'levantar', 'mas', 'la', 'voz', 
+# 'para', 'oirte', 'y', 'se', 'forma', 'un', 'ambiente', 'que', 'no', 'lo', 'que', 'se', 'espera', 'de', 'una', 'estrella', 'michelin', '.']
+# output_sequence = viterbi(test, t_params, e_params, train_words)
+# print('\n')
+# print(output_sequence)
+# print(len(test))
+# print(len(output_sequence))
 ## Actual viterbi
 # prediction = viterbi_loop(es_dev, t_params, e_params, train_words)
 
